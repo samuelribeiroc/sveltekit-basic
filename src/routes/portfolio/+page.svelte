a3ddc3e107ef61887b1ce72af855705afea0eb37
Samuel Costa <samuel.costa@ccc.ufcg.edu.br>
2024-09-06 18:37:01 -0300

    feat: portfolio page

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+  export let data;
+
+  const items = data.items;
+</script>
+
+<section class="bg-white dark:bg-gray-900">
+  <div class="container px-6 py-10 mx-auto">
+      <div class="flex py-4 mt-4 overflow-x-auto overflow-y-hidden md:justify-center dark:border-gray-700">
+          <button
+              class="h-12 px-8 py-2 -mb-px text-sm text-center text-blue-600 bg-transparent border-b-2 border-blue-500 sm:text-base dark:border-blue-400 dark:text-blue-300 whitespace-nowrap focus:outline-none">
+              Animation
+          </button>
+
+          <button
+              class="h-12 px-8 py-2 -mb-px text-sm text-center text-gray-700 bg-transparent border-b-2 border-gray-200 sm:text-base dark:text-white whitespace-nowrap cursor-base focus:outline-none dark:border-gray-700 dark:hover:border-gray-400 hover:border-gray-400">
+              Web design
+          </button>
+
+          <button
+              class="h-12 px-8 py-2 -mb-px text-sm text-center text-gray-700 bg-transparent border-b-2 border-gray-200 sm:text-base dark:text-white whitespace-nowrap cursor-base focus:outline-none dark:border-gray-700 dark:hover:border-gray-400 hover:border-gray-400">
+              App design
+          </button>
+
+          <button
+              class="h-12 px-8 py-2 -mb-px text-sm text-center text-gray-700 bg-transparent border-b-2 border-gray-200 sm:text-base dark:text-white whitespace-nowrap cursor-base focus:outline-none dark:border-gray-700 dark:hover:border-gray-400 hover:border-gray-400">
+              Branding
+          </button>
+      </div>
+
+      <section class="mt-8 space-y-8 lg:mt-12">
+        {#each items as {category, title, src}}
+          <section class="lg:flex lg:items-center">
+            <div class="lg:w-1/2 ">
+              <p class="text-lg tracking-wider text-blue-500 uppercase dark:text-blue-400 ">{category}</p>
+                <h2 class="mt-2 text-2xl font-semibold text-gray-800 capitalize dark:text-white">{title}</h2>
+            </div>
+
+            <div class="mt-4 lg:w-1/2 lg:mt-0">
+              <img class="object-cover w-full h-64 rounded-lg md:h-96"
+                {src}
+                alt="">
+            </div>
+          </section>
+        {/each}
+      </section>
+  </div>
+</section>
---
+++ src/routes/portfolio/+page.ts
@@ -0,0 +1,8 @@
+export function load() {
+  return {
+    items: [
+      { category: "Ui kit", title: "Block of Ui kit collections", src: "https://images.unsplash.com/photo-1621609764180-2ca554a9d6f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" },
+      { category: "Wareframe", title: "Best free website layout", src: "https://images.unsplash.com/photo-1600132806608-231446b2e7af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" }
+    ]
+  }
+}
